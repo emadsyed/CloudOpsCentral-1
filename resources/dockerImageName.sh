a1db866cca690e8ff7bc6e7e15b9393065bca772
Adil <adilforms@gmail.com>
2018-12-14 02:48:42 -0500

    Updating Script

--- resources/dockerImageName.sh
+++ resources/dockerImageName.sh
@@ -1,5 +1,4 @@
  version=$(git rev-parse --short HEAD)
-      P_Name=$(echo $JOB_NAME | tr / . | tr "[:upper:]" "[:lower:]")
-      Package_Name=${P_NAME%.*}
-      echo $Package_Name
-      echo "adilforms/$Package_Name.$version:$BRANCH_NAME"
+ PNAME=$(echo $JOB_NAME | tr / . | tr "[:upper:]" "[:lower:]")
+PACKAGENAME=${PNAME%.*}
+echo $PACKAGENAME
