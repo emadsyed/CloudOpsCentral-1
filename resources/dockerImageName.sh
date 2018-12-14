bccd91c95cde68be4b7cc570c92606ed535be860
Adil <adilforms@gmail.com>
2018-12-14 02:45:57 -0500

    test

--- resources/dockerImageName.sh
+++ resources/dockerImageName.sh
@@ -1,4 +1,5 @@
  version=$(git rev-parse --short HEAD)
       P_Name=$(echo $JOB_NAME | tr / . | tr "[:upper:]" "[:lower:]")
       Package_Name=${PNAME%.*}
+      echo $Package_Name
       echo "adilforms/$Package_Name.$version:$BRANCH_NAME"
