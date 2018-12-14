5034c463746b6fdd7d91c28b99d2d7472a3f839b
Adil <adilforms@gmail.com>
2018-12-14 02:54:34 -0500

    echo $PACKAGENAME

--- resources/dockerBuild.sh
+++ resources/dockerBuild.sh
@@ -1,5 +1,6 @@
 
       # arg $1 is "repo/packageName.version:branchName"
       echo "Building image..."
+      echo $PACKAGENAME
       docker build -t adilforms/$PACKAGENAME.$version:$BRANCH_NAME .
   
