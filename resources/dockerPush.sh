3f92e66f7dff0ff1d0e6a1b579b020bca4b69c5b
Adil <adilforms@gmail.com>
2018-12-14 03:10:59 -0500

    Adding Version

--- resources/dockerPush.sh
+++ resources/dockerPush.sh
@@ -1,6 +1,7 @@
     # arg $1 is "repo/packageName.version:branchName"
       echo "Publishing..."
       #docker push $1
+      version=$(git rev-parse --short HEAD)
       PNAME=$(echo $JOB_NAME | tr / . | tr "[:upper:]" "[:lower:]")
       PACKAGENAME=${PNAME%.*}
       docker push adilforms/$PACKAGENAME.$version:$BRANCH_NAME
