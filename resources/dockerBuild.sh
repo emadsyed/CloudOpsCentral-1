29acc7c41f5fe7d04226bc9504a0cfbcf0c417b8
Adil <adilforms@gmail.com>
2018-12-14 02:50:23 -0500

    Updating Docker Npm Install step

--- resources/dockerBuild.sh
+++ resources/dockerBuild.sh
@@ -1,5 +1,5 @@
 
       # arg $1 is "repo/packageName.version:branchName"
       echo "Building image..."
-      docker build -t $1 .
+      docker build -t adilforms/$PACKAGENAME.$version:$BRANCH_NAME .
   
