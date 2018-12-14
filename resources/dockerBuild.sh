708d3252e99ee089cb979554e3355fcb8a3e24dd
Adil <adilforms@gmail.com>
2018-12-14 02:59:03 -0500

    Adding Docker file step

--- resources/dockerBuild.sh
+++ resources/dockerBuild.sh
@@ -1,6 +1,2 @@
-
-      # arg $1 is "repo/packageName.version:branchName"
-      echo "Building image..."
-      echo $PACKAGENAME
-      docker build -t adilforms/$PACKAGENAME.$version:$BRANCH_NAME .
-  
+echo "Generating Docker File in WorkSpace
+curl https://raw.githubusercontent.com/adilforms/Shared-tempalte/test/Dockerfile > Dockerfile
