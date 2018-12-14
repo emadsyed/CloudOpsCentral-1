d3d86a50327b450d9d9fa58d37fcda51b09f4ada
Adil <adilforms@gmail.com>
2018-12-14 03:08:49 -0500

    Jenkins Not Storing Values from Sh script

--- resources/dockerPush.sh
+++ resources/dockerPush.sh
@@ -1,4 +1,6 @@
     # arg $1 is "repo/packageName.version:branchName"
       echo "Publishing..."
       #docker push $1
+      PNAME=$(echo $JOB_NAME | tr / . | tr "[:upper:]" "[:lower:]")
+      PACKAGENAME=${PNAME%.*}
       docker push adilforms/$PACKAGENAME.$version:$BRANCH_NAME
