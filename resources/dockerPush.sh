2cf583068879857e691569928bb02650d5ccc5e8
Adil <adilforms@gmail.com>
2018-12-14 03:02:04 -0500

    Adding Docker Push Step

--- resources/dockerPush.sh
+++ resources/dockerPush.sh
@@ -1,3 +1,4 @@
     # arg $1 is "repo/packageName.version:branchName"
       echo "Publishing..."
-      docker push $1
+      #docker push $1
+      docker push adilforms/$PACKAGENAME.$version:$BRANCH_NAME
