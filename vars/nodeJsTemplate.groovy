5f2990a0dd7382bd7b62602d82f7ddec2026432d
Adil <adilforms@gmail.com>
2018-12-18 17:01:24 -0500

    testing read function

--- vars/nodeJsTemplate.groovy
+++ vars/nodeJsTemplate.groovy
@@ -6,16 +6,20 @@ def call(Map config) {
   
 stage('Checkout'){
  checkout scm  // In this Step Jenkins will get the Git Url and Branch name from the job.
-  def dockerfile = libraryResource 'dockerfilepull.sh' // Reading Docker function to Copy Docker file.
- sh dockerfile  
+  //def dockerfile = libraryResource 'dockerfilepull.sh' // Reading Docker function to Copy Docker file.
+ //sh  dockerfile  
+ def dockerfile = readfile libraryResource 'DOCKERFILE'
+ def DOCKERFILE = new file './'
+ DOCKERFILE << dockerfile.txt
  
 }
 stage('Build'){
  
     echo 'building'
     sh 'npm install'
- def artifactname = libraryResource 'dockerBuild.sh'
- sh artifactname
+ //def artifactname = libraryResource 'dockerBuild.sh'
+ //sh artifactname
+ 
  
 }
 stage('Test'){ 
