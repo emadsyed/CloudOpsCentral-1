4d62ba8c3116b367a072e9ecc4ec75ce80b758f9
Adil <adilforms@gmail.com>
2018-12-18 10:19:04 -0500

    testing Function

--- vars/nodeJsTemplate.groovy
+++ vars/nodeJsTemplate.groovy
@@ -4,9 +4,9 @@ def command = '''
  PACKAGENAME=${PNAME%.*}
  echo $PACKAGENAME
 '''
- //def proc = ['bash', '-c', PACKAGENAME].execute()
-    // proc.waitFor()
-      // println proc.text
+ def proc = ['bash', '-c', PACKAGENAME].execute()
+    proc.waitFor()
+      println proc.text
 
 
 def call(Map config) {
