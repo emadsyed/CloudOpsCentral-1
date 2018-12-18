5ec4e8e3348bb77fcf110f3d4ea90daab7d6c523
Adil <adilforms@gmail.com>
2018-12-18 15:22:40 -0500

    ''''

--- vars/nodeJsTemplate.groovy
+++ vars/nodeJsTemplate.groovy
@@ -1,12 +1,12 @@
 
 def PACKAGENAME () {
-    sh  """#!/bin/bash -l
+    '''
 
        version=$(git rev-parse --short HEAD)
        PNAME=$(echo $JOB_NAME | tr / . | tr "[:upper:]" "[:lower:]")
        PACKAGENAME=${PNAME%.*}
        echo $PACKAGENAME
-                  """
+                  '''
     return true
 }
 
