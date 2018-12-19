7ed89ad0285d050063430ee7593a7474e9c8b9c6
Adil <adilforms@gmail.com>
2018-12-19 10:28:46 -0500

    fixed typo

--- vars/nodeJsTemplate.groovy
+++ vars/nodeJsTemplate.groovy
@@ -2,7 +2,7 @@ def dockerImageName() {
   def imageName = sh(returnStdout: true, script: """#!/bin/bash -el
       version=\$(git rev-parse --short HEAD)
       P_Name=\$(echo $JOB_NAME | tr / . | tr "[:upper:]" "[:lower:]")
-      Package_Name=\${PNAME%.*}
+      Package_Name=\${P_NAME%.*}
       echo "adilforms/$Package_Name.$version:$BRANCH_NAME"
    """).trim()
 }
