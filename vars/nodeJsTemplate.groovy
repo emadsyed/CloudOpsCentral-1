fccb0348fb52ad718e48e8520b8b5d6035ceaeca
Adil <adilforms@gmail.com>
2018-12-19 01:11:36 -0500

    removed temp file

--- vars/nodeJsTemplate.groovy
+++ vars/nodeJsTemplate.groovy
@@ -33,7 +33,7 @@ stage('Build'){
 stage('Test'){ 
 
      
-def version = sh( script: 'git rev-parse --short HEAD > short-git-sha.tmp', returnStdout: true).toString().trim()
+def version = sh( script: 'git rev-parse --short HEAD', returnStdout: true).toString().trim()
     echo 'using new version ' +  version
     return version
  }
