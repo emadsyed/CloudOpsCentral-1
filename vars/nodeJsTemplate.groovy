be38c63283f6acc8126000b7d13a56c5cf41810e
Adil <adilforms@gmail.com>
2018-12-19 01:10:14 -0500

    test

--- vars/nodeJsTemplate.groovy
+++ vars/nodeJsTemplate.groovy
@@ -3,9 +3,7 @@ import groovy.json.*
  
 def dockerfile = readfile libraryResource 'DOCKERFILE'
  
-def version = sh( script: 'git rev-parse --short HEAD > short-git-sha.tmp', returnStdout: true).toString().trim()
-    echo 'using new version ' +  version
-    return version
+
 
 
 def call(Map config) {
@@ -35,8 +33,9 @@ stage('Build'){
 stage('Test'){ 
 
      
- sh 'Version= echo $version'
- echo '$Version'
+def version = sh( script: 'git rev-parse --short HEAD > short-git-sha.tmp', returnStdout: true).toString().trim()
+    echo 'using new version ' +  version
+    return version
  }
  
 stage('Publish') { 
